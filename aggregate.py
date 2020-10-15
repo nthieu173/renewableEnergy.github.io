ffd78bf2801c5a3a88ce3d7242b34e69ea3c7c2f
Hieu Nguyen <nthieu173@gmail.com>
2020-10-14 21:39:10 -0400

    Aggregate hydropower potential data

--- aggregate.py
+++ aggregate.py
@@ -7,44 +7,54 @@ states_names = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA",
         "SD","TN","TX","UT","VT","VA","WA","WV","WI","WY","DC"]
 
 def add_solar_data(data, year_from, year_to):
-    with open("solar_radiance/us_states_yearly_average_dni.csv") as solar_file:
-        solar_reader = csv.DictReader(solar_file)
-        for row in solar_reader:
+    with open("solar_radiance/us_states_yearly_average_dni.csv") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
             for year in range(year_from, year_to+1):
                 data[(row["State"], year)]["dni"] = float(row[str(year)])
     return data
 
 def add_precipitation_data(data, year_from, year_to):
-    with open("weather/us_states_annual_average_precipitation.csv") as solar_file:
-        solar_reader = csv.DictReader(solar_file)
-        for row in solar_reader:
+    with open("weather/us_states_annual_average_precipitation.csv") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
             for year in range(year_from, year_to+1):
                 data[(row["State"], year)]["precipitation"] = float(row[str(year)])
     return data
 
 def add_snowfall_data(data, year_from, year_to):
-    with open("weather/us_states_annual_average_snowfall.csv") as solar_file:
-        solar_reader = csv.DictReader(solar_file)
-        for row in solar_reader:
+    with open("weather/us_states_annual_average_snowfall.csv") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
             for year in range(year_from, year_to+1):
                 data[(row["State"], year)]["snowfall"] = float(row[str(year)])
     return data
 
 def add_temperature_data(data, year_from, year_to):
-    with open("weather/us_states_annual_average_temperature.csv") as solar_file:
-        solar_reader = csv.DictReader(solar_file)
-        for row in solar_reader:
+    with open("weather/us_states_annual_average_temperature.csv") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
             for year in range(year_from, year_to+1):
                 data[(row["State"], year)]["temperature"] = float(row[str(year)])
     return data
 
 def add_windspeed_data(data, year_from, year_to):
-    with open("weather/us_states_annual_average_windspeed.csv") as solar_file:
-        solar_reader = csv.DictReader(solar_file)
-        for row in solar_reader:
+    with open("weather/us_states_annual_average_windspeed.csv") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
             for year in range(year_from, year_to+1):
                 data[(row["State"], year)]["windspeed"] = float(row[str(year)])
     return data
+
+def add_hydropower_potential_data(data, year_from, year_to):
+    with open("hydropower_potential/us_states_hydropower_potential.csv") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            for year in range(year_from, year_to+1):
+                data[(row["State"], year)]["potential_hydro_cap"] = int(row["PotentialCapacityMW"])
+                data[(row["State"], year)]["potential_hydro_gen"] = int(row["PotentialGenerationGWHYR"])
+    return data
+
 
 def main():
     year_from = 1998
@@ -53,21 +63,21 @@ def main():
     for state in states_names:
         for year in range(year_from, year_to+1):
             data[(state, year)] = {}
-
     data = add_solar_data(data, year_from, year_to)
     data = add_precipitation_data(data, year_from, year_to)
     data = add_snowfall_data(data, year_from, year_to)
     data = add_temperature_data(data, year_from, year_to)
     data = add_windspeed_data(data, year_from, year_to)
+    data = add_hydropower_potential_data(data, year_from, year_to)
 
-    features = ["dni", "precipitation", "snowfall", "temperature", "windspeed"]
+    features = ["dni", "precipitation", "snowfall", "temperature", "windspeed","potential_hydro_cap","potential_hydro_gen"]
 
     with open("aggregated_us_states_year.csv", "w", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(["State", "Year"]+features)
         for (name, year) in data:
             state_data = data[(name, year)]
-            row = [state, year]
+            row = [name, year]
             for feature in features:
                 row += [state_data[feature]]
             csvwriter.writerow(row)
