30df8c49af098fa440777057d93051ece1f8e950
Hieu Nguyen <nthieu173@gmail.com>
2020-10-24 20:58:57 -0400

    PCA colored by renewable percentage

--- pca.py
+++ pca.py
@@ -2,10 +2,13 @@ import numpy as np
 from sklearn.decomposition import PCA
 import pandas as pd
 from matplotlib import pyplot as plt
+from matplotlib import colors
 
 df = pd.read_csv('new_consolidated_data.csv', sep=',',header=0)
 
 features = df.iloc[:,2:-13]
+
+renew_percent = df.iloc[:,-1] / 100
 
 pca = PCA(n_components=3, svd_solver='full')# In[25]:
 
@@ -15,5 +18,11 @@ reduced = pca.transform(features)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(reduced[:,0],reduced[:,1],reduced[:,2])
+
+n = len(renew_percent)
+color_percent = np.vstack((np.zeros(n),renew_percent.to_numpy(),np.zeros(n))).T
+row_color = np.apply_along_axis(colors.to_hex, 1, color_percent)
+
+
+ax.scatter(reduced[:,0],reduced[:,1],reduced[:,2],c=row_color)
 plt.show()
